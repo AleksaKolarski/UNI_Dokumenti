4dffc33cbe08be82b0eb9b389d90b61acbb35afa
AleksaPC <aleksa.kolarski@yahoo.com>
2018-12-15 12:26:23 +0100

    Initial commit

---
+++ src/main/java/com/projekat/dokumenti/DokumentiApplication.java
@@ -0,0 +1,14 @@
+package com.projekat.dokumenti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DokumentiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DokumentiApplication.class, args);
+	}
+
+}
+
