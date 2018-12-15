12c42e800ead9fc4d8b4cbd6ab5d01c144ab068c
AleksaPC <aleksa.kolarski@yahoo.com>
2018-12-15 20:59:43 +0100

    Dodata podrska za logovanje.

--- src/main/java/com/projekat/dokumenti/DokumentiApplication.java
+++ src/main/java/com/projekat/dokumenti/DokumentiApplication.java
@@ -1,12 +1,18 @@
 package com.projekat.dokumenti;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DokumentiApplication {
 
+	static Logger log = LogManager.getLogger(DokumentiApplication.class);
+
 	public static void main(String[] args) {
+		
+		log.info("Pokrenuta aplikacija");
 		SpringApplication.run(DokumentiApplication.class, args);
 	}
 
