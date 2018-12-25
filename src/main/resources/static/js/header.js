43bf30e7890011d4d034e85d052405544c568002
DELL <aleksa.kolarski@yahoo.com>
2018-12-25 22:15:05 +0100

    SASS

--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -1,10 +1,23 @@
+/*
+**
+**
+**
+*/
+
 $(document).ready(function(e){
   var header;
+  var navigation;
 
   header = $('#id_header');
+  navigation = $('#id_header_navigation');
 
   customAjax("GET", "auth/current-user", null, function(data, status, xhr){
     console.log(data);
+    data.roles.forEach(role => {
+      if(role.name == 'ROLE_ADMIN'){
+        navigation.append('<button type="button">Korisnici</button>');
+      }
+    });
   },
   function(xhr, status){
     
